92d187319bfb950c59f809b7610605f89e093d36
agnelrayan <agnelrayan@gmail.com>
2018-05-29 16:47:51 +0530

    29-may-2018

---
+++ appletex/AppletEx.java
@@ -0,0 +1,16 @@
+package com.expertzlab.appletex;
+
+import java.applet.Applet;
+import java.awt.*;
+
+/**
+ * Created by agnel on 5/29/18.
+ */
+public class AppletEx extends Applet {
+
+    public void paint(Graphics g){
+        g.drawString("Welcome to Cochin",50,50);
+    }
+
+
+}
