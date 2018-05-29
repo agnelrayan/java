d418f7a8cbbf54804765b121bc88e35ff58d0bcf
agnelrayan <agnelrayan@gmail.com>
2018-05-29 16:31:03 +0530

    29-may-2018

---
+++ AbstractCar.java
@@ -0,0 +1,10 @@
+package com.expertzlab.interfaceEx;
+
+/**
+ * Created by agnel on 2/28/18.
+ */
+public abstract class AbstractCar implements Cars{
+    public void drive(){
+        System.out.println("Drive in Abstract--");
+    }
+}
---
+++ InterfaceEx.java
@@ -0,0 +1,40 @@
+package com.expertzlab.interfaceEx;
+public interface InterfaceEx
+{
+    void sound();
+    void walk();
+    void speak();
+
+}
+
+class ClassEx implements InterfaceEx{
+    public void sound(){
+
+        System.out.println("In Sound Method");
+
+    }
+    public void walk(){
+
+        System.out.println("In Walk Method");
+    }
+public void speak(){
+    System.out.println("In Speak Method");
+
+}
+    public void smile(){
+        System.out.println("In Smile Method");
+    }
+    public static void main(String[] args) {
+        InterfaceEx obj = new ClassEx();
+        obj.sound();
+        obj.walk();
+        ClassEx obj2 = new ClassEx();
+        obj2.smile();
+        //obj.smile();
+    }
+
+
+
+}
+
+
