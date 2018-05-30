e80b6ec0aeb30752d96af705cdb17e96bb6481e5
agnelrayan <agnelrayan@gmail.com>
2018-05-30 16:01:19 +0530

    30-may-2018

---
+++ interfaceEx/AeroPlane.java
@@ -0,0 +1,14 @@
+package com.expertzlab.interfaceEx;
+
+/**
+ * Created by agnel on 5/30/18.
+ */
+public class AeroPlane extends SkyTravel{
+
+    public AeroPlane(){
+        System.out.println("In AeroPlan Constructor...");
+    }
+    void quantity() {
+        System.out.println("Quantity in AeroPlane");
+    }
+}
---
+++ interfaceEx/Helicopter.java
@@ -0,0 +1,14 @@
+package com.expertzlab.interfaceEx;
+
+/**
+ * Created by agnel on 5/30/18.
+ */
+public class Helicopter extends SkyTravel {
+    void quantity() {
+        System.out.println("Quantity in Helicopter");
+    }
+
+    void message(){
+        System.out.println("Message in Helicopter...");
+    }
+}
